c5e7ac57fa89f65502cca0e32bc1e2d30be08d55
Paul Sijpkes <paul.sijpkes@gmail.com>
2025-10-15 17:09:11 +1100

    Update canvas-dp2md.user.js

--- canvas-dp2md.user.js
+++ canvas-dp2md.user.js
@@ -1,5 +1,15 @@
-
-
+// ==UserScript==
+// @name         Canvas DesignPlus to Markdown
+// @namespace    http://tampermonkey.net/
+// @version      1.6
+// @description  Convert to or from DesignPLUS HTML in Canvas to or from Markdown with custom markers
+// @author       Paul Sijpkes
+// @match        https://*/courses/*/pages/*/edit
+// @match        https://*/courses/*/discussion_topics/*/edit
+// @grant        GM_setClipboard
+// @updateURL    https://raw.githubusercontent.com/sijpkes/userscripts/main/canvas-dp2md.meta.js
+// @downloadURL  https://raw.githubusercontent.com/sijpkes/userscripts/main/canvas-dp2md.user.js
+// ==/UserScript==
 
 (function() {
     'use strict';
