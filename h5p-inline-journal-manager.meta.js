31aa4b976d43cb472f3ebb2f9066073b74e16c64
Paul Sijpkes <paul.sijpkes@gmail.com>
2026-02-06 20:31:12 +1100

    Add metadata for Canvas H5P Journal Manager script

---
+++ h5p-inline-journal-manager.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Canvas H5P Journal Manager
+// @namespace    http://tampermonkey.net/
+// @version      3.4
+// @description  Link Canvas to H5P. UI elements are removed automatically on Save.
+// @author       Paul
+// @match        https://canvas.newcastle.edu.au/courses/*/pages/*/edit*
+// @match        https://uonline.h5p.com/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_log
+// @require      file:///C:\Users\ps158\workspace\tampermonkey_userscripts\h5p-inline-journal-manager.js
+// ==/UserScript==
